d852239a13be4719221f31638c6641e5d57c7cef
ander1dw@gmail.com <ander1dw@gmail.com@afe51d7e-41a2-113c-a6f6-545e3b13914c>
2011-11-05 16:53:00 +0000

    Initial commit.

---
+++ trunk/constants.py
@@ -0,0 +1,3 @@
+API_URL = 'http://football.myfantasyleague.com/%d/export?TYPE=nflSchedule&W=%d'
+CURRENT_WEEK = 9
+DEPLOY_DATE = 'Nov 3 2011'
---
+++ trunk/models.py
@@ -0,0 +1,47 @@
+from google.appengine.ext import db
+
+class Team(db.Model):
+   location = db.StringProperty()
+   nickname = db.StringProperty()
+   last_updated = db.DateTimeProperty(auto_now=True)
+
+class Record(db.Model):
+   year = db.IntegerProperty()
+   team = db.ReferenceProperty(Team, collection_name="records")
+   wins = db.IntegerProperty()
+   losses = db.IntegerProperty()
+   ties = db.IntegerProperty()
+   last_updated = db.DateTimeProperty(auto_now=True)
+        
+class Matchup(db.Model):
+   year = db.IntegerProperty()
+   week = db.IntegerProperty()
+   team = db.ReferenceProperty(Team, collection_name="matchups")
+   score = db.IntegerProperty()
+   opp_team = db.StringProperty()
+   opp_score = db.IntegerProperty()
+   kickoff = db.StringProperty()
+   at_home = db.BooleanProperty()
+   result = db.StringProperty()
+   last_updated = db.DateTimeProperty(auto_now=True)
+    
+class Ranking(db.Model):
+   year = db.IntegerProperty()
+   week = db.IntegerProperty()
+   rank = db.IntegerProperty()
+   team = db.ReferenceProperty(Team, collection_name="rankings")
+   user = db.UserProperty()
+   last_updated = db.DateTimeProperty(auto_now=True)
+    
+class AverageRanking(db.Model):
+   year = db.IntegerProperty()
+   week = db.IntegerProperty()
+   rank = db.FloatProperty()
+   team = db.ReferenceProperty(Team, collection_name="averages")
+   last_updated = db.DateTimeProperty(auto_now=True)
+   
+class UserProfile(db.Model):
+   user = db.UserProperty()
+   nickname = db.StringProperty()
+   display_avg = db.BooleanProperty()
+   last_updated = db.DateTimeProperty(auto_now=True)
