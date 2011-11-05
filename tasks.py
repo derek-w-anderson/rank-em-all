4c285e70c2ef676d4ecd8d6ff10375d494d69ae5
ander1dw <ander1dw@gmail.com>
2011-11-05 16:53:00 +0000

    Initial commit.

---
+++ tasks.py
@@ -0,0 +1,215 @@
+import webapp2 as web
+from urllib2 import urlopen
+from google.appengine.ext import db
+from google.appengine.api import users
+from datetime import datetime
+from time import strftime, gmtime
+from xml.dom import minidom
+from constants import *
+from models import *
+
+class RankingCopier(web.RequestHandler):
+   def get(self):
+      from_year = int(self.request.get('from_year'))
+      from_week = int(self.request.get('from_week'))
+      to_year = int(self.request.get('to_year'))
+      to_week = int(self.request.get('to_week'))
+
+      # Copy rankings from one week to another:
+      rankings = Ranking.all().filter('year =', from_year).filter('week =', from_week)
+      for ranking in rankings:
+         key_name = str(to_year)+'-'+str(to_week)+'-'+ranking.team.key().name()+'-'+str(ranking.user.user_id())
+         copied_ranking = Ranking.get_or_insert(
+               key_name, # 2011-8-ARI-ander1dw
+               year=to_year,
+               week=to_week,
+               team=ranking.team,
+               user=ranking.user,
+            )
+         copied_ranking.rank = ranking.rank
+         copied_ranking.put()
+
+      # Update the average ranking:
+      teams = Team.all().order('nickname')
+      for team in teams:
+         rankings = team.rankings.filter('year =', to_year).filter('week =', to_week)
+         if rankings:
+            avg = AverageRanking.get_or_insert(
+               str(to_year)+'-'+str(to_week)+'-'+team.key().name(), # 2011-8-ARI
+               year=to_year,
+               week=to_week,
+               team=team
+            )
+            sum = 0
+            for ranking in rankings:
+               sum += ranking.rank
+            avg.rank = round(sum / float(rankings.count()), 1)
+            avg.put()
+
+      self.response.headers['Content-Type'] = 'text/plain'
+      self.response.out.write('Rankings copied successfully.')
+
+
+class AverageRankingCalculator(web.RequestHandler):
+   def get(self):
+      current_year = datetime.now().year
+      week = int(self.request.get('week'))
+
+      teams = Team.all().order('nickname')
+      for team in teams:
+         rankings = team.rankings.filter('year =', current_year).filter('week =', week)
+         if rankings:
+            avg = AverageRanking.get_or_insert(
+               str(current_year)+'-'+str(week)+'-'+team.key().name(), # 2011-8-ARI
+               year=current_year,
+               week=week,
+               team=team
+            )
+            sum = 0
+            for ranking in rankings:
+               sum += ranking.rank
+            avg.rank = round(sum / float(rankings.count()), 1)
+            avg.put()
+
+      self.response.headers['Content-Type'] = 'text/plain'
+      self.response.out.write('Averages calculated successfully.')
+
+
+class MatchupAndRecordUpdater(web.RequestHandler):
+   def get(self):
+      offset = 4 * (60 * 60)  # adjusts kickoff time to ET
+      current_year = datetime.now().year
+
+      teams = {}
+      for week in range(1, CURRENT_WEEK+1):
+         url = API_URL % (current_year, week)
+         xml = minidom.parse(urlopen(url))
+
+         for matchup in xml.getElementsByTagName('matchup'):
+            kickoff = int(matchup.getAttribute('kickoff')) - offset
+            team1 = matchup.getElementsByTagName('team')[0]
+            team1_id = team1.getAttribute('id')
+            team2 = matchup.getElementsByTagName('team')[1]
+            team2_id = team2.getAttribute('id')
+
+            if not teams.has_key(team1_id): teams[team1_id] = {}
+            if not teams.has_key(team2_id): teams[team2_id] = {}
+            if not teams[team1_id].has_key('weeks'): teams[team1_id]['weeks'] = {}
+            if not teams[team1_id].has_key('win'):   teams[team1_id]['win'] = 0
+            if not teams[team1_id].has_key('loss'):  teams[team1_id]['loss'] = 0
+            if not teams[team1_id].has_key('tie'):   teams[team1_id]['tie'] = 0
+            if not teams[team2_id].has_key('weeks'): teams[team2_id]['weeks'] = {}
+            if not teams[team2_id].has_key('win'):   teams[team2_id]['win'] = 0
+            if not teams[team2_id].has_key('loss'):  teams[team2_id]['loss'] = 0
+            if not teams[team2_id].has_key('tie'):   teams[team2_id]['tie'] = 0
+
+            teams[team1_id]['weeks'][week] = {
+               'score': 0,
+               'opp_team': team2_id,
+               'opp_score': 0,
+               'kickoff': strftime("%a %I:%M %p ET", gmtime(kickoff)),
+               'at_home': True if team1.getAttribute('isHome') == '1' else False,
+               'result': None
+            }
+            teams[team2_id]['weeks'][week] = {
+               'score': 0,
+               'opp_team': team1_id,
+               'opp_score': 0,
+               'kickoff': strftime("%a %I:%M %p ET", gmtime(kickoff)),
+               'at_home': True if team2.getAttribute('isHome') == '1' else False,
+               'result': None
+            }
+            if int(matchup.getAttribute('gameSecondsRemaining')) == 0:
+               teams[team1_id]['weeks'][week]['score'] = int(team1.getAttribute('score'))
+               teams[team1_id]['weeks'][week]['opp_score'] = int(team2.getAttribute('score'))
+               teams[team2_id]['weeks'][week]['score'] = int(team2.getAttribute('score'))
+               teams[team2_id]['weeks'][week]['opp_score'] = int(team1.getAttribute('score'))
+
+               if int(team1.getAttribute('score')) > int(team2.getAttribute('score')):
+                  teams[team1_id]['weeks'][week]['result'] = 'W'
+                  teams[team1_id]['win'] += 1
+                  teams[team2_id]['weeks'][week]['result'] = 'L'
+                  teams[team2_id]['loss'] += 1
+
+               elif int(team1.getAttribute('score')) < int(team2.getAttribute('score')):
+                  teams[team1_id]['weeks'][week]['result'] = 'L'
+                  teams[team1_id]['loss'] += 1
+                  teams[team2_id]['weeks'][week]['result'] = 'W'
+                  teams[team2_id]['win'] += 1
+
+               else:
+                  teams[team1_id]['weeks'][week]['result'] = 'T'
+                  teams[team1_id]['tie'] += 1
+                  teams[team2_id]['weeks'][week]['result'] = 'T'
+                  teams[team2_id]['tie'] += 1
+
+      for team_id in teams.keys():
+         team = teams[team_id]
+         team_obj = Team.get_by_key_name(team_id)
+
+         record = Record.get_or_insert(
+            str(current_year)+'-'+team_id, # 2011-ARI
+            year=current_year,
+            team=team_obj,
+         )
+         record.wins = team['win']
+         record.losses = team['loss']
+         record.ties = team['tie']
+         record.put()
+
+         for week in team['weeks'].keys():
+            matchup = Matchup.get_or_insert(
+               str(current_year)+'-'+str(week)+'-'+team_id, # 2011-8-ARI
+               year=current_year,
+               week=week,
+               team=team_obj,
+               opp_team=team['weeks'][week]['opp_team'],
+               kickoff=team['weeks'][week]['kickoff'],
+               at_home=team['weeks'][week]['at_home']
+            )
+            matchup.score = team['weeks'][week]['score']
+            matchup.opp_score = team['weeks'][week]['opp_score']
+            matchup.result = team['weeks'][week]['result']
+            matchup.put()
+
+      self.response.headers['Content-Type'] = 'text/plain'
+      self.response.out.write('Teams updated successfully.')
+
+
+class TeamLoader(web.RequestHandler):
+   def get(self):
+      Team.get_or_insert('ARI', location='Arizona', nickname='Cardinals')
+      Team.get_or_insert('BAL', location='Baltimore', nickname='Ravens')
+      Team.get_or_insert('ATL', location='Atlanta', nickname='Falcons')
+      Team.get_or_insert('DET', location='Detroit', nickname='Lions')
+      Team.get_or_insert('BUF', location='Buffalo', nickname='Bills')
+      Team.get_or_insert('CIN', location='Cincinatti', nickname='Bengals')
+      Team.get_or_insert('IND', location='Indianapolis', nickname='Colts')
+      Team.get_or_insert('TEN', location='Tennessee', nickname='Titans')
+      Team.get_or_insert('MIA', location='Miami', nickname='Dolphins')
+      Team.get_or_insert('NYJ', location='New York', nickname='Jets')
+      Team.get_or_insert('CLE', location='Cleveland', nickname='Browns')
+      Team.get_or_insert('HOU', location='Houston', nickname='Texans')
+      Team.get_or_insert('JAC', location='Jacksonville', nickname='Jaguars')
+      Team.get_or_insert('MIN', location='Minnesota', nickname='Vikings')
+      Team.get_or_insert('CHI', location='Chicago', nickname='Bears')
+      Team.get_or_insert('CAR', location='Carolina', nickname='Panthers')
+      Team.get_or_insert('TBB', location='Tampa Bay', nickname='Buccaneers')
+      Team.get_or_insert('KCC', location='Kansas City', nickname='Chiefs')
+      Team.get_or_insert('DEN', location='Denver', nickname='Broncos')
+      Team.get_or_insert('DAL', location='Dallas', nickname='Cowboys')
+      Team.get_or_insert('NYG', location='New York', nickname='Giants')
+      Team.get_or_insert('SEA', location='Seattle', nickname='Seahawks')
+      Team.get_or_insert('STL', location='St. Louis', nickname='Rams')
+      Team.get_or_insert('SFO', location='San Francisco', nickname='49ers')
+      Team.get_or_insert('NEP', location='New England', nickname='Patriots')
+      Team.get_or_insert('PIT', location='Pittsburgh', nickname='Steelers')
+      Team.get_or_insert('PHI', location='Philadeplhia', nickname='Eagles')
+      Team.get_or_insert('WAS', location='Washington', nickname='Redskins')
+      Team.get_or_insert('GBP', location='Green Bay', nickname='Packers')
+      Team.get_or_insert('SDC', location='San Diego', nickname='Chargers')
+      Team.get_or_insert('OAK', location='Oakland', nickname='Raiders')
+      Team.get_or_insert('NOS', location='New Orleans', nickname='Saints')
+
+      self.response.headers['Content-Type'] = 'text/plain'
+      self.response.out.write('Teams loaded successfully.')
---
+++ views.py
@@ -0,0 +1,215 @@
+import webapp2 as web
+import urllib2 
+import jinja2
+import os
+from google.appengine.ext import db
+from google.appengine.api import users
+from datetime import datetime
+from xml.dom import minidom
+from constants import *
+from models import *
+from tasks import *
+
+jinja_environment = jinja2.Environment(
+   loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+class RankingPage(web.RequestHandler):
+   def get(self):
+      current_year = datetime.now().year
+      week = self.request.get('week')
+      has_prev_ranks = False
+      display_avg = False
+      
+      # Check parameters:
+      if not week:
+         week = CURRENT_WEEK 
+      else:
+         try:
+            week = int(week)
+            if week < 1 or week > CURRENT_WEEK:
+               raise ValueError
+         except ValueError:
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.response.out.write('Bad parameter.')
+            return
+         
+      user = users.get_current_user()
+      if user:
+         profile = UserProfile.get_by_key_name(user.user_id())
+         if profile:
+            display_avg = profile.display_avg 
+
+         rankings = {}
+         teams = Team.all().order('nickname')
+         for i, team in enumerate(teams):
+            # Get team record:
+            record_obj = team.records.filter('year =', current_year).get()
+            record = str(record_obj.wins) + '-' + str(record_obj.losses)
+            if (record_obj.ties > 0):
+               record += '-' + str(record_obj.ties)
+            
+            # Get team matchup:
+            match_obj = team.matchups.filter('year =', current_year).filter('week =', week).get()
+            matchup = ''
+            if match_obj and match_obj.result:
+               span_class = ''
+               if match_obj.result == 'W':
+                  span_class = 'green' 
+               elif match_obj.result == 'L':
+                  span_class = 'red'
+               matchup = '<span class="' + span_class + '">' + match_obj.result + '</span> ' 
+               matchup += str(match_obj.score) + '-' + str(match_obj.opp_score) + ' '
+            
+            if match_obj and not match_obj.at_home:
+               matchup += '@' + match_obj.opp_team
+            elif match_obj:
+               matchup += match_obj.opp_team
+            else:
+               matchup += 'Bye'
+                           
+            # Get saved ranking: 
+            rank_obj = team.rankings.filter('year =', current_year).filter('week =', week).filter('user =', user).get()      
+            rank = (i + 1) if not rank_obj else rank_obj.rank
+            
+            # Get previous week's ranking:
+            prev_rank = None
+            if week > 1:
+               prev_rank_obj = team.rankings.filter('year =', current_year).filter('week =', week-1).filter('user =', user).get()      
+               if prev_rank_obj:
+                  prev_rank = prev_rank_obj.rank
+                  has_prev_ranks = True
+            
+            # Get average ranking:
+            avg_rank_obj = team.averages.filter('year =', current_year).filter('week =', week).get()
+            average = float(i + 1) if not avg_rank_obj else avg_rank_obj.rank
+            
+            rankings[rank] = {
+               'team_id': team.key().name(),
+               'team_name': team.location + ' ' + team.nickname,
+               'prev_rank': prev_rank,
+               'average': average,
+               'record': record,
+               'matchup': matchup
+            }            
+         template = jinja_environment.get_template('index.html')
+         self.response.out.write(template.render({
+            'rankings': rankings,
+            'has_prev_ranks': has_prev_ranks,
+            'display_avg': display_avg,
+            'current_week': CURRENT_WEEK,
+            'selected_week': week,
+            'export_cell': str(chr(98+week)).upper(),
+            'status': self.request.get('status'),
+            'last_updated': DEPLOY_DATE
+         }))
+            
+      else:
+         self.redirect(users.create_login_url(self.request.uri))
+  
+   def post(self):
+      current_year = datetime.now().year
+      week = self.request.get('week')
+      
+      # Check parameters:
+      if not week:
+         week = CURRENT_WEEK 
+      else:
+         try:
+            week = int(week)
+            if week < 1 or week > CURRENT_WEEK:
+               raise ValueError
+         except ValueError:
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.response.out.write('Bad parameter.')
+            return
+            
+      user = users.get_current_user()
+      if user:
+         teams = Team.all().order('nickname')
+
+         # Store submitted rankings:
+         for team in teams:
+            key_name = str(current_year)+'-'+str(week)+'-'+team.key().name()+'-'+str(user.user_id())
+            ranking = Ranking.get_or_insert(
+               key_name, # 2011-8-ARI-ander1dw
+               year=current_year,
+               week=week,
+               team=team,
+               user=user
+            )
+            submitted_rank = self.request.POST['hidden-'+team.key().name()]
+            if not submitted_rank:
+               submitted_rank = -1
+            else:
+               try:
+                  submitted_rank = int(submitted_rank)
+                  if submitted_rank < 1 or submitted_rank > 32:
+                     raise ValueError
+               except ValueError:
+                  submitted_rank = -1
+            ranking.rank = submitted_rank 
+            ranking.put()
+
+         # Update the average ranking:            
+         for team in teams:
+            rankings = team.rankings.filter('year =', current_year).filter('week =', week)
+            if rankings:
+               avg = AverageRanking.get_or_insert(
+                  str(current_year)+'-'+str(week)+'-'+team.key().name(), # 2011-8-ARI
+                  year=current_year,
+                  week=week,
+                  team=team
+               )
+               sum = 0
+               for ranking in rankings:
+                  sum += ranking.rank
+               avg.rank = round(sum / float(rankings.count()), 1)
+               avg.put()
+            
+         self.redirect('/rank?week='+str(week)+'&status=saved')
+            
+      else:
+         self.redirect(users.create_login_url(self.request.uri))
+  
+  
+class DisplayAverageUpdater(web.RequestHandler):
+   def post(self):
+      user = users.get_current_user()
+      week = self.request.POST['week']
+      display_avg = self.request.POST['display_avg']
+
+      # Check parameters:
+      if not user or not week or not display_avg:
+         self.response.headers['Content-Type'] = 'text/plain'
+         self.response.out.write('Bad POST data.')
+         return
+         
+      try:
+         week = int(week)
+         if week < 1 or week > CURRENT_WEEK:
+            raise ValueError
+            
+         if display_avg not in ('Y', 'N'):
+            raise ValueError
+          
+         profile = UserProfile.get_or_insert(user.user_id())
+         profile.display_avg = ('Y' == display_avg)
+         profile.put()
+            
+      except ValueError:
+         self.response.headers['Content-Type'] = 'text/plain'
+         self.response.out.write('Bad parameter.')
+         return
+      
+      self.redirect('/rank?week='+str(week))
+  
+  
+urls = [
+   ('/rank', RankingPage),
+   ('/update_display_avg', DisplayAverageUpdater),
+   ('/tasks/update_teams', MatchupAndRecordUpdater),
+   ('/tasks/load_teams', TeamLoader),
+   ('/tasks/calculate_averages', AverageRankingCalculator),
+   ('/tasks/copy_rankings', RankingCopier)
+]
+app = web.WSGIApplication(urls, debug=False)
